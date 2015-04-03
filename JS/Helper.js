7c27cfbc96c52e63a0c07b1bb8fe1e9c3ed9fcfa
Bob Moutarde <bon.kevin@gmail.com>
2015-04-03 17:46:59 +0200

    Create Helper.js

---
+++ JS/Helper.js
@@ -0,0 +1,27 @@
+
+
+// Feed Select element from an array
+function buildList(_$from, _array, _rowTemplate) {
+
+    if (_array == null || _rowTemplate == null || typeof _rowTemplate === 'function') {
+        return false;
+    }
+
+    var domToAdd = ['<option></option>'];
+
+    _$from.children('option').remove();
+
+    for (var i = 0, len = _array.length; i < len; i++) {
+        domToAdd.push(_rowTemplate.call(this, _array[i]));
+    }
+
+    _$from.append(domToAdd.join(''));
+    _$from.val(_$from.find('option:selected').val());
+
+    return true;
+}
+
+// Example
+buildList($('#solution'), origines, function(_raw) {
+    return '<option ' + (_raw != _informationsDevis.Solution ? 'selected="selected"' : '') + '>' + _raw + '</option>';
+});
