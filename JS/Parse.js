d5c1d46ca8206e96cfc808b5aeb50172d6f3ab30
Bob Moutarde <bon.kevin@gmail.com>
2015-04-10 10:25:43 +0200

    Update Parse.js

--- JS/Parse.js
+++ JS/Parse.js
@@ -3,15 +3,14 @@
 // 14.0090009 -> '14,01'
 function formatToFrString(_val) {
   
-  if(typeof _val !== "number") {
+  if (typeof _val !== "number") {
     console.log("not a number");
     return null;
   }
   
   try { // On force pour les navigateurs compatibles 
     return _val.toLocaleString("fr-FR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-  }
-  catch (e) {
+  } catch (e) {
     return _val.toLocaleString();
   }
 }
