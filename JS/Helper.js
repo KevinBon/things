6d2d8bf983cc94765907de574c06bba4070ea112
Bob Moutarde <bon.kevin@gmail.com>
2015-04-24 23:21:10 +0200

    Update Helper.js

--- JS/Helper.js
+++ JS/Helper.js
@@ -53,3 +53,17 @@ buildList($('#solution'), origines, function(_raw) {
         selected: dummy === _raw.on
     };
 });
+
+// --
+(function(myApp) {
+  myApp.init = function() {
+    // kick off your code
+  };
+
+  myApp.handleClick = function() {}; // etc...
+}(window.myApp = window.myApp || {}));
+
+// Only include at end of main application...
+$(document).ready(function() {
+  window.myApp.init();
+});
